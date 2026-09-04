bcc2194fc4c376d29921d428e3c1cc3278666cc0
Roshini7788 <mandapalliroshini@gmail.com>
2026-09-04 17:18:30 +0530

    updated udyamsetu changes

--- frontend/lib/theme/app_theme.dart
+++ frontend/lib/theme/app_theme.dart
@@ -68,10 +68,10 @@ class AppTheme {
           ),
         ),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: surfaceCard,
         elevation: 2,
-        shadowColor: Colors.black.withOpacity(0.06),
+        shadowColor: Colors.black.withValues(alpha: 0.06),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16),
         ),
